477bc7a1b7186cc362fd557861a5eae6d2d40f4b
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-05-10 13:52:09 +0200

    Add links constants

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const links = [
+	{ title: 'Domů', destination: '#domu' },
+	{ title: 'O závodě', destination: '#o-zavode' },
+	{ title: 'Fotogalerie', destination: '#fotogalerie' },
+	{ title: 'Registrace', destination: '#registrace' },
+	{ title: 'Mapa', destination: '#mapa' },
+	{ title: 'Sponzoři', destination: '#sponzori' },
+];
+
+export const socialSites = [
+	{ title: 'Facebook', link: 'https://www.facebook.com/events/1065790363770438/' },
+	{ title: 'Instagram', destination: 'https://www.instagram.com/waterbouldergames/' },
+];
