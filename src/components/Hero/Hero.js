3cb8ce0f4d80f7e4189bb6fb4a32032b3f7aeada
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-05-10 13:53:22 +0200

    Add hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ import climber from '../../Images/climber.png';
 
 function Hero() {
 	return (
-		<section className={styles.container}>
+		<section id="domu" className={styles.container}>
 			<div className={styles.wrapper}>
 				<img src={logo} alt="Logo" id={styles.logo} />
 				<h1>Zažij jediné závody v lezení nad vodou v ČR!</h1>
