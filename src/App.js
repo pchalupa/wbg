36a9d79c0af17c907ad5a3be8baa056056392279
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-05-10 14:37:14 +0200

    Set page layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hero from './components/Hero/Hero';
+import About from './components/Article/About';
+import Instagram from './components/Gallery/Instagram';
+import ContactForm from './components/Form/Contactform';
+import GoogleMaps from './components/Map/GoogleMaps';
+import MainMenu from './components/Menu/MainMenu';
+import Logos from './components/Gallery/Logos';
+import { links, socialSites } from './constants';
+import styles from './App.module.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<MainMenu links={links} socialSites={socialSites} />
+			<main className={styles.wrapper}>
+				<Hero />
+				<About />
+				<Instagram count={0} />
+				<ContactForm />
+				<GoogleMaps />
+				<Logos />
+			</main>
+		</div>
+	);
 }
 
 export default App;
