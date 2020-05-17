b79ffff508ab79f4ae3e5901a69d330a0d031e11
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-05-17 12:29:22 +0200

    Add video link

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Hero.module.scss';
 import logo from '../../Images/logo.svg';
+import { FaRegPlayCircle } from 'react-icons/fa';
 import climber from '../../Images/climber.png';
 
 function Hero() {
@@ -9,6 +10,9 @@ function Hero() {
 			<div className={styles.wrapper}>
 				<img src={logo} alt="Logo" id={styles.logo} />
 				<h1>Zažij jediné závody v lezení nad vodou v ČR!</h1>
+				<a href="https://www.facebook.com/waterbouldergames/videos/413009075775419/" className={styles.video}>
+					<FaRegPlayCircle />
+				</a>
 			</div>
 			<img src={climber} alt="climber" id={styles.hero} />
 		</section>
