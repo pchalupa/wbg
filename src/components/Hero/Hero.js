a0754c1e92016c4099af9115a12b93f669f75828
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-05-17 19:26:25 +0200

    Set image to lazy load

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,13 +8,13 @@ function Hero() {
 	return (
 		<section id="domu" className={styles.container}>
 			<div className={styles.wrapper}>
-				<img src={logo} alt="Logo" id={styles.logo} />
+				<img src={logo} alt="Logo" id={styles.logo} loading="lazy" />
 				<h1>Zažij jediné závody v lezení nad vodou v ČR!</h1>
 				<a href="https://www.facebook.com/waterbouldergames/videos/413009075775419/" className={styles.video}>
 					<FaRegPlayCircle />
 				</a>
 			</div>
-			<img src={climber} alt="climber" id={styles.hero} />
+			<img src={climber} alt="climber" id={styles.hero} loading="lazy" />
 		</section>
 	);
 }
