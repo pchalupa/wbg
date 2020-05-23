121b280e4a686926f5ca9ce03232b9e12c9aa2d1
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-05-23 11:10:55 +0200

    Add timeline data

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const links = [
 	{ title: 'Domů', destination: '#domu' },
 	{ title: 'O závodě', destination: '#o-zavode' },
 	{ title: 'Fotogalerie', destination: '#fotogalerie' },
-	{ title: 'Registrace', destination: '#registrace' },
+	{ title: 'Vítězové', destination: '#vitezove' },
 	{ title: 'Mapa', destination: '#mapa' },
 	{ title: 'Sponzoři', destination: '#sponzori' },
 ];
@@ -10,4 +10,32 @@ export const links = [
 export const socialSites = [
 	{ title: 'Facebook', link: 'https://www.facebook.com/events/1065790363770438/' },
 	{ title: 'Instagram', destination: 'https://www.instagram.com/waterbouldergames/' },
+];
+
+export const winners = [
+	{
+		title: 2015,
+		man: ['Štěpán Stráník', 'Vilda Chejn', 'Marek Schaffer'],
+		woman: ['Eliška Vlčková', 'Eliška Handlířová', 'Michaela Matúšová'],
+	},
+	{
+		title: 2016,
+		man: ['Rishat Khaibullin', 'Martin Spilka', 'Martin Spilka'],
+		woman: ['Martin Spilka', 'Michaela Matúšová', 'Nikola Janoušková'],
+	},
+	{
+		title: 2017,
+		man: ['Rishat Khaibullin ', 'Martin Stráník', 'Michal Běhounek'],
+		woman: ['Dominika Dupalová', 'Petra Růžičková', 'Michaela Matúšová'],
+	},
+	{
+		title: 2018,
+		man: ['Jiří Bradáč', 'Petr Handlíř', 'Richard Pařil'],
+		woman: ['Adéla Bajerová', 'Kateřina Valeková', 'Hana Štorkanová'],
+	},
+	{
+		title: 2019,
+		man: ['Martin Dvorský', 'Jirka Bradáč', 'Matouš Bradáč'],
+		woman: ['Dominika Dupalová', 'Áďa Bajerová', 'Háňa Štorkánová'],
+	},
 ];
