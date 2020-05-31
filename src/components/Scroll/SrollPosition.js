f702c386cb98eec947fc08ed46be5c2ded4b8dc0
Petr Chalupa <petrchalupa@Petr-MacBook-Pro.local>
2020-05-31 20:25:22 +0200

    Rename component

--- src/components/Scroll/SrollPosition.js
+++ src/components/Scroll/SrollPosition.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import styles from './Bar.module.scss';
+import styles from './SrollPosition.module.scss';
 
-function Bar() {
+function SrollPosition() {
 	const getPosition = () => {
 		return Math.round((window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100);
 	};
@@ -15,4 +15,4 @@ function Bar() {
 	return <div className={styles.container} style={{ width: `${scrollPosition}%` }}></div>;
 }
 
-export default Bar;
+export default SrollPosition;
